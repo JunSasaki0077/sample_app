31f07f5bdf923d90e1fb6d3af9e6cc90c5a40f1d
Junsasaki0077 <ikasas0077@gmail.com>
2020-10-29 08:02:44 +0000

    Finish 11

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,12 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  
+   host = '72de641bb15942a782b9cff31e1b66db.vfs.cloud9.ap-northeast-1.amazonaws.com' 
+   # ここをコピペすると失敗します。
+   #自分の環境のホストに変えてください。
+   # クラウドIDEの場合は以下をお使いください
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
 
